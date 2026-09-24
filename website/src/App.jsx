839e4bc777e5f49d868bb86bcf18b6ee284b8d77
mindhackcommittee <mindhackcommittee@gmail.com>
2026-09-23 23:09:41 -0400

    website template finished

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -9,25 +9,31 @@ import Schedule from './pages/Schedule'
 function App() {
     return (
         <>
-            <h>MindHack Carleton</h>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/registration">Register</Link>
-                <Link to="/schedule">Schedule</Link>
-                <Link to="/executives">Executive Team</Link>
-                <Link to="/gallery">Gallery</Link>
-            </nav>
+            <div class="navbar">
+                <div class="logodiv">
+                    <img class="logo" src="TPMH2027Logo.png"></img>
+                    <h>MindHack Carleton</h>
+                </div>
 
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/registration" element={<Registration />} />
-                <Route path="/schedule" element={<Schedule/>} />
-                <Route path="/executives" element={<Executives />} />
-                <Route path="/gallery" element={<Gallery />} />
-            </Routes>
-            <p>End Of Page</p>
+                <nav>
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/registration">Register</Link>
+                    <Link to="/schedule">Schedule</Link>
+                    <Link to="/executives">Executive Team</Link>
+                    <Link to="/gallery">Gallery</Link>
+                </nav>
+            </div>
+            <div class="page">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/registration" element={<Registration />} />
+                    <Route path="/schedule" element={<Schedule/>} />
+                    <Route path="/executives" element={<Executives />} />
+                    <Route path="/gallery" element={<Gallery />} />
+                </Routes>
+            </div>
         </>
     )
 }
